9323e0c6fb39ff1996452be024b4e6d33104c768
JP <jp.sherka@gmail.com>
2024-08-04 01:07:55 -0400

    add navbar and drawer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import { ThemeContextProvider } from './context/ThemeContext';
 import { SnackbarContextProvider } from './context/SnackbarContext';
 import ToggleColorMode from './components/ToggleColorMode';
+import NavBar from './components/NavBar';
+import DrawerComponent from './components/DrawerComponent';
 
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
+  const handleDrawerToggle = (newOpen) => {
+    setDrawerOpen(newOpen);
+  };
 
   return (
     <ThemeContextProvider>
       <SnackbarContextProvider>
-      <>
-        <ToggleColorMode/>
-        <h> sea of dice</h>
-      </>
+        <>
+          <ToggleColorMode />
+          <NavBar toggleDrawer={handleDrawerToggle} />
+          <DrawerComponent openDrawer={drawerOpen} toggleDrawer={handleDrawerToggle} />
+        </>
       </SnackbarContextProvider>
     </ThemeContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
